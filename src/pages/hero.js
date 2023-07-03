cc7413458c689c4ac9ec1d6190580c3ab0db9e2b
David Ryan <david.ryan@huskers.unl.edu>
2023-07-04 03:13:09 +0530

    started adding custom stuff

---
+++ src/pages/hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { HeroSection, Page, Section, Animation } from 'gatsby-theme-portfolio-minimal';
+
+export default function IndexPage() {
+    return (
+        <Page>
+            <HeroSection sectionId="hero" />
+            <Animation type="fadeUp">
+                <Section heading="This is a brand new section.">
+                    <p>You can add your custom JSX here.</p>
+                </Section>
+            </Animation>
+        </Page>
+    );
+}
